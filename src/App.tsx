d05666654d291d518e7016f009d5921b9111753f
gruba <grubavijay@gmail.com>
2023-05-14 16:38:06 +0530

    updated route

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,16 @@ const App:React.FC=()=> {
     
       <div className="bg-gray-900 p-5 font-serif">
         <h1 className=" text-amber-600 text-5xl text-center">Contact Management App </h1>
-      
+        
       </div>
+     
     
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Test />}></Route> 
-          <Route path='/contactform' element={<AddContact />}></Route> 
-          <Route path='/edit/:cid' element={<EditContact />}></Route> 
-          <Route path='/delete' element={<DeleteContact />}></Route> 
+          <Route path='/cma_proj' element={<Test />}></Route> 
+          <Route path='/cma_proj/contactform' element={<AddContact />}></Route> 
+          <Route path='/cma_proj/edit/:cid' element={<EditContact />}></Route> 
+          <Route path='/cma_proj/delete' element={<DeleteContact />}></Route> 
         </Routes>
       </BrowserRouter>
    
